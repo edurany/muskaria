1c1e4747c86e9e02c0e7fbe0f5e364cd564ab4dc
Edurany <enricdurany@gmail.com>
2020-02-12 00:29:08 +0100

    Google Tag Manager
    Adding code

--- header.php
+++ header.php
@@ -6,6 +6,13 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">	
 	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-PPXLFGX');</script>
+	<!-- End Google Tag Manager -->
 	<?php wp_head();?>		 
 </head>
 <?php 
@@ -13,6 +20,10 @@
 	$alith_widget_header_style = get_theme_mod('alith_widget_header_style');	
 ?>
 <body <?php body_class(array($alith_header_style, $alith_widget_header_style));?>>	
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PPXLFGX"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <?php if (get_theme_mod('alith_boxed_layout')):?>	
 	<div class="main-wrap">
 <?php endif; ?>	
